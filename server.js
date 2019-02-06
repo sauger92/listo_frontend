31a03c974a1722c8f6143dff6d46c869a359d501
Jerem <jeremy.bouhi@edu.ece.fr>
2019-02-06 22:25:18 +0100

    aller la

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist/listo_frontend'));
+app.use(express.static(__dirname + '/dist/listo_frontend'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/index.html'));
+res.sendFile(path.join(__dirname+'/dist/listo_frontend/index.html'));
 });
 
 
