a5561ddf31cc327f3fff8eef8f95305a4820bcd5
lucasTrevalinet <lucas.trevalinet@gmail.com>
2019-02-07 00:36:36 +0100

    NAV BAR OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { AuthService } from './services/auth.service';
 import { Component } from '@angular/core';
 import { TripService } from './services/trip.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +10,40 @@ import { TripService } from './services/trip.service';
 })
 export class AppComponent {
   title = 'listo';
+  isAuth : boolean;
+  visibility : any
 
-  constructor(private appareilService: TripService) {
+  constructor(private appareilService: TripService,private authService:AuthService, private router: Router) {
   
   }
   
   ngOnInit() {
-    
+    this.isAuth = this.authService.LoginStatue;
   }
+
+  Deconexion(){
+    this.authService.unlogUser();
+    this.isAuth = this.authService.LoginStatue;
+    if (this.isAuth == false)
+    {
+      console.log("Deconnexion de l'utilisateur");
+      this.router.navigate(['/login']);
+    } 
+  
+  }
+
+  getVisibility()
+  {
+    if(this.authService.LoginStatue == false)
+    {
+      this.visibility = 'hidden'
+    }
+    else if (this.authService.LoginStatue == true) 
+    {
+      this.visibility = 'visible';
+    }
+    return this.visibility;
+  }
+
+
 }
